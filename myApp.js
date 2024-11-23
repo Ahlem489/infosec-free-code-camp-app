0877797446e2022cd27b9233182062a14920d0e1
Ahlem489 <ftouhaahlem@gmail.com>
2024-11-23 22:11:37 +0100

    My initial Application

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+const PORT = process.env.PORT || 3030;
+
 
 
 
@@ -58,5 +60,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`🥦Useful Programmer Info Security App Started on Port ${PORT}`);
 });
